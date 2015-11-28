65ae720be54a021f834161f6a34a4d08d14004ab
Matthew Butler <butler.matthew@gmail.com>
2015-11-28 09:50:00 -0400

    Add header

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -4,6 +4,9 @@ library dart_slides.main.app;
 import 'package:polymer/polymer.dart';
 import 'package:web_components/web_components.dart' show HtmlImport;
 
+import 'package:polymer_elements/paper_header_panel.dart';
+import 'package:polymer_elements/paper_toolbar.dart';
+
 @PolymerRegister('main-app')
 class MainApp extends PolymerElement {
   MainApp.created() : super.created();
--- web/index.dart
+++ web/index.dart
@@ -2,6 +2,8 @@ library dart_slides.main;
 
 import 'package:polymer/polymer.dart';
 
+import 'package:yummy_wookie/main_app.dart'; // Uses [MainApp]
+
 main() async {
   await initPolymer();
 }
